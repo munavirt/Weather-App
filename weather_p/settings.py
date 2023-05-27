e065f940eadd3eab90f9b77074018362105f736e
munavirt <munavirt79@gmail.com>
2023-05-27 12:33:26 +0530

    aws update

--- weather_p/settings.py
+++ weather_p/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-908(%z#=wzo63qed%%t8+je!5qa3a!_&@0gq@^@0m3xm-)=jt$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
